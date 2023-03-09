16c4b814f2b5a5fbbc51bb87745eef5e777351d3
eliel <elielluwal9@gmail.com>
2023-03-09 22:55:23 +0100

    first

---
+++ chat/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Post)
+admin.site.register(EmployeeGroup)
+admin.site.register(MessageGroupe)
+admin.site.register(Employee)
+admin.site.register(Message)
+admin.site.register(ContentMessage)
+admin.site.register(Call)
+admin.site.register(CallType)
---
+++ chat/models.py
@@ -0,0 +1,78 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=255, null=False)
+    mail = models.EmailField(null=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContentMessage(models.Model):
+    text_message = models.TextField(null=True)
+    file = models.FileField(upload_to='send-files')
+
+    @property
+    def check_content(self):
+        if self.text_message is None:
+            return False  # si le contenu n'est pas un message
+        return True  # si le contenu est un message
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, related_name='sender')
+    receiver = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, related_name='receiver')
+    message_content = models.ForeignKey(ContentMessage, on_delete=models.CASCADE)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class EmployeeGroup(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class MessageGroupe(models.Model):
+    employee_group = models.ForeignKey(EmployeeGroup, on_delete=models.CASCADE)
+    sender = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False, related_name='sender1')
+    receivers = models.ManyToManyField(Employee, related_name='receivers1')
+    message_content = models.ForeignKey(ContentMessage, on_delete=models.CASCADE)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CallType(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Call(models.Model):
+    caller = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='caller')
+    receivers = models.ManyToManyField(Employee, related_name='receivers')
+    type = models.ForeignKey(CallType, on_delete=models.CASCADE)
+    at_time = models.DateTimeField(auto_now_add=True)
+    close_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.caller.name
